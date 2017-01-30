9a6c0b76c453b26e9292078c24b3fa8ca781f2f5
Nebez Briefkani <me@nebezb.com>
2017-01-29 21:11:14 -0500

    Create Info controller for returning App ID/Api Key

---
+++ app/Controllers/InfoController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+use Mvc\Application;
+use Symfony\Component\HttpFoundation\Request;
+
+class InfoController {
+
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+    public function info(Request $request)
+    {
+        return $this->app->json([
+            'appId' => env('ALGOLIA_APP_ID'),
+            'apiKey' => env('ALGOLIA_API_KEY')
+        ]);
+    }
+
+}
--- app/routes.php
+++ app/routes.php
@@ -1,4 +1,5 @@
 <?php
 
+$app->get('/api/1/info', 'App\Controllers\InfoController@info');
 $app->get('/api/1/apps', 'App\Controllers\TestController@test');
 $app->get('/api/1/apps/:id', 'App\Controllers\TestController@testId');
