e6a4e606df00e2793295f3a89a115829a48c473c
Nebez Briefkani <me@nebezb.com>
2017-01-29 21:44:56 -0500

    Initialize Algolia PHP client

--- app/Indexers/AlgoliaItemIndexer.php
+++ app/Indexers/AlgoliaItemIndexer.php
@@ -3,11 +3,15 @@
 namespace App\Indexers;
 
 use App\Contracts\ItemIndexer;
+use AlgoliaSearch\Client as ApiClient;
 
 class AlgoliaItemIndexer implements ItemIndexer {
+    private $apiClient;
+
     public function __construct()
     {
-        // ... Create an API client
+        $this->apiClient = new ApiClient(env('ALGOLIA_APP_ID'), env('ALGOLIA_API_KEY'));
+        $this->apiClient->initIndex('apps');
     }
 
     public function addItemToIndex($index, $item)
