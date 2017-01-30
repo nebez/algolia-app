9ddf22ab6b2dd13ead40dd731b803f0b346255c8
Nebez Briefkani <me@nebezb.com>
2017-01-30 00:13:29 -0500

    Fix misleading typo

--- app/env.php
+++ app/env.php
@@ -4,8 +4,8 @@ $dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
 
 $dotenv->load();
 
-// Prevent loading the application if these two environment variables are set.
-// They're necessary for proper functionality!
+// Prevent loading the application if these two environment variables are not
+// set. They're necessary for proper functionality!
 $dotenv->required([
     'ALGOLIA_APP_ID',
     'ALGOLIA_API_KEY'
